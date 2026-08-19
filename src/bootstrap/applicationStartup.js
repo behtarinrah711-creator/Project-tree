1eb048bd14f2d86178ecc8b1bd5d0a32baa12c64
behtarinrah711-creator <behtarinrah711@gmail.com>
2026-08-20 01:29:59 +0330

    Isolate shell controls from application startup

---
+++ src/bootstrap/applicationStartup.js
@@ -0,0 +1,33 @@
+import { appRouter } from '../core/router.js';
+import { moduleRegistry } from '../core/moduleRegistry.js';
+import { projectContext } from '../core/projectContext.js';
+import { projectRepository } from '../data/projectRepository.js';
+import { projectModules } from '../modules/index.js';
+import { listProjects, getProject, getActiveProject, selectProject } from '../core/projectWorkspace.js';
+import { taskRuntimeModule } from '../modules/tasks/taskRuntimeModule.js';
+import { loadLegacyRuntime } from './legacyBootstrap.js';
+
+/** Start the modular API, then the classic legacy runtime, then routing. */
+export async function startApplication({
+  windowRef = window,
+  registry = moduleRegistry,
+  modules = projectModules,
+  router = appRouter,
+  loadLegacy = loadLegacyRuntime,
+} = {}){
+  modules.forEach(moduleDefinition => registry.register(moduleDefinition));
+
+  const application = Object.freeze({
+    modules: registry,
+    projectContext,
+    projectRepository,
+    taskRuntime: taskRuntimeModule,
+    projectWorkspace: Object.freeze({ listProjects, getProject, getActiveProject, selectProject }),
+  });
+  windowRef.KarhaApp = application;
+
+  await loadLegacy();
+  router.start();
+  windowRef.dispatchEvent(new windowRef.CustomEvent('karha:ready'));
+  return application;
+}
