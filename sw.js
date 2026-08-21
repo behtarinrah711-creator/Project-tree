727965f481297945ac1081bcd62a4d16cc2fd446
behtarinrah711-creator <behtarinrah711@gmail.com>
2026-08-21 17:03:42 +0330

    Restore exact known-good site state before PR #54

--- sw.js
+++ sw.js
@@ -1,23 +1,31 @@
+const RUNTIME_CACHE = 'karha-runtime';
+
 self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    try {
-      const keys = await caches.keys();
-      await Promise.all(keys.map(key => caches.delete(key)));
-    } catch (e) {}
-    try {
-      await self.registration.unregister();
-    } catch (e) {}
-    try {
-      const clients = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
-      clients.forEach(client => client.navigate(client.url));
-    } catch (e) {}
-  })());
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys
+        .filter(key => key !== RUNTIME_CACHE)
+        .map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener('fetch', () => {
-  // Emergency rollback mode: do not intercept requests.
+self.addEventListener('fetch', (event) => {
+  if(event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request, { cache:'no-store' })
+      .then(response => {
+        if(response.ok && new URL(event.request.url).origin === self.location.origin){
+          const copy=response.clone();
+          event.waitUntil(caches.open(RUNTIME_CACHE).then(cache => cache.put(event.request,copy)));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
