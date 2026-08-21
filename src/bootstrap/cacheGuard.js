cb0f1be3e73028ceb614c6077ab5d2dbb4f30cf2
behtarinrah711-creator <behtarinrah711@gmail.com>
2026-08-21 17:00:58 +0330

    Emergency reset browser caches after rollback

--- src/bootstrap/cacheGuard.js
+++ src/bootstrap/cacheGuard.js
@@ -1,6 +1,25 @@
 (function(){
-  if(!('serviceWorker' in navigator)) return;
-  navigator.serviceWorker.getRegistrations()
-    .then(registrations => Promise.all(registrations.map(registration => registration.update())))
-    .catch(()=>{});
+  const RESET_KEY='karha-emergency-cache-reset-v1';
+  if(sessionStorage.getItem(RESET_KEY)) return;
+  sessionStorage.setItem(RESET_KEY,'1');
+
+  const clearWorkers = ('serviceWorker' in navigator)
+    ? navigator.serviceWorker.getRegistrations()
+        .then(registrations => Promise.all(registrations.map(registration => registration.unregister())))
+        .catch(()=>{})
+    : Promise.resolve();
+
+  const clearCaches = ('caches' in window)
+    ? caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).catch(()=>{})
+    : Promise.resolve();
+
+  Promise.all([clearWorkers, clearCaches]).then(()=>{
+    try{
+      const url=new URL(location.href);
+      url.searchParams.set('_karha_reset','1');
+      location.replace(url.toString());
+    }catch(e){
+      location.reload();
+    }
+  });
 })();
